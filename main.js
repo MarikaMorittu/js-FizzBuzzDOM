0adbdae70b5a86639da3998a5f44fa0bd4b373ad
MarikaMorittu <marikaegio@hotmail.it>
2022-05-26 19:35:46 +0200

    create FizzBuzzDOM

---
+++ main.js
@@ -0,0 +1,24 @@
+let container = document.querySelector(".container");
+// pongo la condizione con il ciclo for
+for (i = 1; i <= 100; i++) {
+  // continuo con le singole condizioni all'interno del ciclo
+  if (i % 3 == 0) {
+    if (i % 15 == 0) {
+      // crea un div e scrivici dentro FizzBuzz
+      let box = `<div class="box fizzbuzz">FizzBuzz</div>`;
+      container.innerHTML += box;
+    } else {
+      // crea un div e scrivici dentro Fizz
+      let box = `<div class="box fizz">Fizz</div>`;
+      container.innerHTML += box;
+    }
+  } else if (i % 5 == 0) {
+    // crea un div e scrivici dentro Buzz
+    let box = `<div class="box buzz">Buzz</div>`;
+    container.innerHTML += box;
+  } else {
+    // crea un div e scrivici dentro il valore di i
+    let box = `<div class="box">${i}</div>`;
+    container.innerHTML += box;
+  }
+}
